d6a2c16789be82c97f29cf4d782eeab496c3f15c
Sarveksha Manoj Chambhare <sarvekshachambhare@gmail.com>
2025-09-23 23:12:52 +0530

    first commit

---
+++ basics/Demo2.java
@@ -0,0 +1,9 @@
+class Demo2
+{
+	public static void main(String[] args)
+	{
+		System.out.print("First Statement ");
+		System.out.print("Second Statement ");
+		System.out.print("Third Statement ");		
+	}
+}
---
+++ basics/Demo4.java
@@ -0,0 +1,7 @@
+class Demo4
+{
+	public static void main (String[] args)
+	{
+		System.out.println("Inside Demo4 class");
+	}
+}
