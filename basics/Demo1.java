37af36651ae6c4742a9ebcf267f39d46fb7be0a4
Sarveksha Manoj Chambhare <97785780+Sarveksha21@users.noreply.github.com>
2025-10-01 23:05:17 +0530

    Delete basics directory

--- basics/Demo1.java
+++
@@ -1,8 +0,0 @@
-
-class Demo1
-{
-	public static void main(String[] args)
-	{
-		System.out.print("First Java Program");	
-	}
-}
--- basics/Demo3.java
+++
@@ -1,9 +0,0 @@
-class Demo3
-{
-	public static void main(String[] args)
-	{
-		System.out.println("First Statement");
-		System.out.println("Second Statement");
-		System.out.println("Third Statement");		
-	}
-}
