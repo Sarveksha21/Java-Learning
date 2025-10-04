f85a17828fd18d39ea9de8fe8580971021375e25
Sarveksha Manoj Chambhare <97785780+Sarveksha21@users.noreply.github.com>
2025-10-04 20:34:34 +0530

    Add files via upload

---
+++ operators/For1.java
@@ -0,0 +1,14 @@
+// Write a java program to print values from 1 to 5 in ascending order.
+
+class For1
+{
+	public static void main(String[] args)
+	{
+		System.out.println("Start");
+		for(int i=1; i<=3; i++)
+		{
+			System.out.println("Value of i = "+i);
+		}
+		System.out.println("Stop");
+	}
+}
