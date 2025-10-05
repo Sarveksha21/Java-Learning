b455bb5094f711f7405afaca653893f582bec735
Sarveksha Manoj Chambhare <97785780+Sarveksha21@users.noreply.github.com>
2025-10-05 19:32:25 +0530

    Add files via upload

---
+++ ExerciseQuestions/Ex2.java
@@ -0,0 +1,21 @@
+
+/*	Ex2: Sum of Two Numbers
+
+	Write a Java program to print the sum of two numbers.
+	Test Data: 74 + 36
+	Expected Output : 110
+*/
+
+class Ex2
+{
+	public static void main (String[] args)
+	{
+		System.out.println(74 + 36);
+	}
+}
+
+/*
+	In the exercise above:
+		-This Java code defines a class called 'Exercise2' with a 'main' method. 
+		-When executed, it prints the result of the arithmetic expression '74 + 36' to the console, which is '110'.
+*/
