139fe9ec37162977453e1063368ce1da954d2e72
Sarveksha Manoj Chambhare <97785780+Sarveksha21@users.noreply.github.com>
2025-10-05 19:32:52 +0530

    Add files via upload

---
+++ ExerciseQuestions/Ex3.java
@@ -0,0 +1,22 @@
+
+/*	Ex3: Division of Two Numbers
+
+	Write a Java program to divide two numbers and print them on the screen.
+	Test Data: 50/3
+	Expected Output : 16
+*/
+
+class Ex3
+{
+	public static void main (String[] args)
+	{
+		System.out.println(50/3);
+	}
+}
+
+/*
+	In the exercise above:
+		-The above Java code defines a class called "Exercise3" with a "main()" method.
+		-When executed, it prints the result of the division operation 50 / 3 to the console, which is approximately 16.6667.
+		-Since it's integer division, it will be truncated to 16.
+*/
