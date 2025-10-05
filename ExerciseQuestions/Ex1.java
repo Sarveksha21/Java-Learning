a63ae30ed0dba22b655d75b1788dcee7453286ad
Sarveksha Manoj Chambhare <97785780+Sarveksha21@users.noreply.github.com>
2025-10-05 19:31:32 +0530

    Add files via upload

---
+++ ExerciseQuestions/Ex1.java
@@ -0,0 +1,23 @@
+
+/*	Ex1:
+
+	Write a Java program to print 'Hello' on screen and your name on a separate line.
+	Expected Output :
+			Hello
+			Alexandra Abramov
+*/
+
+class Ex1
+{
+	public static void main (String[] args)
+	{
+		System.out.println("Hello\nAlexandra Abramov");
+	}
+}
+
+/*
+	In the exercise above:
+		-The "main()" method is the program entry point.
+		-It uses System.out.println to print the message.
+		-The message consists of two lines: "Hello" and "Alexandra Abramov!" separated by a newline character (\n).
+*/
