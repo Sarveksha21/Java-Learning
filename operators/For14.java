95fbc6280f7eadd47fa41c36db31dbf62e5fa384
Sarveksha Manoj Chambhare <97785780+Sarveksha21@users.noreply.github.com>
2025-10-07 23:52:29 +0530

    Add files via upload

---
+++ operators/For14.java
@@ -0,0 +1,58 @@
+/*
+Pattern : 
+		 4 5 6 
+		 2 3
+		 1
+*/
+
+class For14
+{
+	public static void main(String[] args)
+	{
+		int k=6;
+		System.out.println("Start");
+		for(int i=3; i>=1; i--)
+		{
+			for(int j=1; j<=i; j++)
+			{
+				System.out.print(" "+k);
+				k--;
+			}
+			System.out.println();
+		}
+		System.out.println("Stop");
+	}
+}		
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
