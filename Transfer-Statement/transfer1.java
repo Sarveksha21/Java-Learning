f80f22776dbcf266bdd0fbf459348889c2b19a70
Sarveksha Manoj Chambhare <97785780+Sarveksha21@users.noreply.github.com>
2025-10-08 19:33:21 +0530

    Add files via upload

---
+++ Transfer-Statement/transfer1.java
@@ -0,0 +1,16 @@
+// Break Statement 
+
+class transfer1
+{
+	public static void main (String[] args)
+	{
+		int i=3;
+		if(i==3)
+		{
+			System.out.println("3 is present");
+			break;
+		}
+	}
+}
+
+// The break statement is responsible to break the flow of the loop and not of the condition.
---
+++ Transfer-Statement/transfer2.java
@@ -0,0 +1,20 @@
+// Continue Statement
+
+class transfer2
+{
+	public static void main (String[] args)
+	{
+		for(int i=1; i<=5; i++)
+		{
+			if(i==3)
+			{
+				System.out.println("The value of i = "+i);
+				continue;
+			}
+		}
+	}
+}
+		
+/*
+	The continue statement is responsible to skip all the statement and move the control at the end of for loop.
+*/
