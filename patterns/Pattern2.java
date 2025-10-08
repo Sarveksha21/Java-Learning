928ee6edbdad6d26dcdc72bd40e443c3fd56658e
Sarveksha Manoj Chambhare <97785780+Sarveksha21@users.noreply.github.com>
2025-10-08 20:38:56 +0530

    Add files via upload

---
+++ patterns/Pattern2.java
@@ -0,0 +1,21 @@
+/*
+Pattern: 1
+		 2 2
+		 3 3 3
+*/
+
+class Pattern2
+{
+	public static void main (String[] args)
+	{
+		for(int i=1; i<=3; i++)
+		{
+			for(int j=1; j<=i; j++)
+			{
+				System.out.print(" "+i);
+			}
+			System.out.println();
+		}	
+		System.out.println("Stop");
+	}
+}
