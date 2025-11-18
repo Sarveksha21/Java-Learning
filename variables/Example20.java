b32409fc85d7d0560115ae622721488f8dff8081
Sarveksha Manoj Chambhare <97785780+Sarveksha21@users.noreply.github.com>
2025-11-18 16:05:06 +0530

    Delete variables/Example20.java

--- variables/Example20.java
+++
@@ -1,8 +0,0 @@
-class Example20
-{
-	public static void main (String[] args)
-	{
-		boolean var = 1;
-		System.out.println(var);	// boolean datatype can't store integer value, hence it will throw an error
-	}
-}
