63ad09cc23bf370021ec47940df89e65aaf5f4f1
Sarveksha Manoj Chambhare <97785780+Sarveksha21@users.noreply.github.com>
2025-11-28 18:56:57 +0530

    Add files via upload

---
+++ StaticBlocks/StaticBlock6.java
@@ -0,0 +1,38 @@
+class StaticBlock6
+{
+	public static void main(String[] args)
+	{
+		System.out.println("Start");
+		Hdfc ref1 = new Hdfc(10000);
+		ref1.getAmountBal();
+		System.out.println("Stop");
+	}
+}
+class Hdfc
+{
+	int amountBal;
+	static		//Static Block
+	{
+		System.out.println("Logic to establish connection with Adhaar Server");
+	}
+	{			//Non-Static Block
+		System.out.println("Logic to verify Adhaar ID & OTP");
+	}
+	Hdfc(int amountBal)		// Constructor
+	{
+		if(amountBal>=1000)
+		{
+			this.amountBal = amountBal;
+			System.out.println("Account is Successfully Created");
+		}
+		else
+		{
+			System.out.println("Invalid Amount, Pleae re-ente valid amount");
+		}
+	}
+	void getAmountBal()		// non-static method
+	{
+		System.out.println("Your Account Balance = "+amountBal);
+	}
+}
+	
