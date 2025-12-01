9b1e877bf9bcb42f4a6f13cc061fb6c91128fe43
Sarveksha Manoj Chambhare <97785780+Sarveksha21@users.noreply.github.com>
2025-12-01 22:10:46 +0530

    Add files via upload

---
+++ variables/LocalVariables/LocalVar1.java
@@ -0,0 +1,8 @@
+class LocalVar1
+{
+	public static void main(String[] args)
+	{
+		int a=100;
+		System.out.println("value of a = "+a);
+	}
+}
---
+++ variables/LocalVariables/LocalVar2.java
@@ -0,0 +1,8 @@
+class LocalVar2
+{
+	public static void main(String[] args)
+	{
+		int a;
+		System.out.println("value of a = "+a);
+	}
+}
