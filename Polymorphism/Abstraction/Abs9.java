f0c817561a5fda8c0e3d2f1382fc40eae924351d
Sarveksha Manoj Chambhare <97785780+Sarveksha21@users.noreply.github.com>
2025-12-30 22:14:15 +0530

    Add files via upload

---
+++ Polymorphism/Abstraction/Abs9.java
@@ -0,0 +1,14 @@
+abstract class Demo
+{
+	int a = 100;
+}
+class Abs9
+{
+	public static void main(String[] args)
+	{
+		Abs9 ref = new Abs9();
+		System.out.pritln(ref.a);
+	}
+}
+
+// Variables are allowed within abstract class, variables are also part of inheritance.
