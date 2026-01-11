ba95403cc3ff35263aba0ded82d4416d206a900a
Sarveksha Manoj Chambhare <97785780+Sarveksha21@users.noreply.github.com>
2026-01-11 23:26:04 +0530

    Add files via upload

---
+++ Polymorphism/StringClasses/Ex18.java
@@ -0,0 +1,19 @@
+class Ex18
+{
+	public static void main(String[] args)
+	{ 
+		String s1 = "CyberSuccess";
+		System.out.println(s1.toUpperCase());	
+		System.out.println(s1.toLowerCase());	
+		String s2 = " eye ";
+		System.out.println(s2);			
+		System.out.println(s2.length());	
+		String s3 = s2.trim();
+		System.out.println(s3);			
+		System.out.println(s3.length());			
+	}
+}
+//subString(), toUpperCase(), toLowerCase(), trim()
+//public java.lang.String toUpperCase;
+//public java.lang.String toLowerCase;
+//public java.lang.String trim;
---
+++ Polymorphism/StringClasses/Ex19.java
@@ -0,0 +1,14 @@
+class Ex19
+{
+	public static void main(String[] args)
+	{ 
+		String s1 = "CyberSuccessTraining";
+		System.out.println(s1.substring(5));	
+		//System.out.println(s1.substring(50));//StringIndexOutOfBoundsException	
+		System.out.println(s1.substring(5,11));	
+
+	}
+}
+
+//public java.lang.String subString(int);
+//public java.lang.String subString(int, int);
