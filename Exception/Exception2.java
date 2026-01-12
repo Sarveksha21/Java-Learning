bd37b56d3b450d308690b52c0e24810fff98192c
Sarveksha Manoj Chambhare <97785780+Sarveksha21@users.noreply.github.com>
2026-01-12 09:34:43 +0530

    Add files via upload

---
+++ Exception/Exception2.java
@@ -0,0 +1,11 @@
+class Exception2
+{
+	public static void main(String[] args)
+	{
+		System.out.println("Start");
+		int[] a = {10, 20, 30};
+		System.out.println(a[10]);		// UnChecked Exception: ArrayOutOfBoundException
+		System.out.println("Start");
+	}
+}
+
